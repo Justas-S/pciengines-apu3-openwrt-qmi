b9202c7997fdd7ec074d2ceabf85bedd9d25f8c5
Justas-S <justas963@gmail.com>
2022-03-01 21:24:34 +0100

    Deal with unnecessaryh quotes

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -ex
+set -e
 
 if [ "$#" -lt 2 ]; then
     echo "Usage $0 [openwrt version]  [arch]";
@@ -7,11 +7,11 @@ if [ "$#" -lt 2 ]; then
 fi
 openwrtVersion="$1"
 arch="$2"
-archPath="$(echo \"$arch\" | sed s/-/\\//)"
+archPath="$(echo $arch | sed s/-/\\//)"
 
 sudo apt install -y build-essential libncurses5-dev libncursesw5-dev zlib1g-dev gawk git gettext libssl-dev xsltproc rsync wget unzip python
 
-wget https://downloads.openwrt.org/releases/$openwrtVersion/targets/$archPath/openwrt-imagebuilder-$openwrtVersion-$arch.Linux-x86_64.tar.xz
+wget "https://downloads.openwrt.org/releases/$openwrtVersion/targets/$archPath/openwrt-imagebuilder-$openwrtVersion-$arch.Linux-x86_64.tar.xz"
 tar -J -x -f openwrt-imagebuilder-*.tar.xz
 cd openwrt-imagebuilder-*/
 

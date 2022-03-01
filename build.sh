07d4cb5588263acc2ceab0b7ea131c1103d96384
Justas <justas963@gmail.com>
2022-03-01 21:13:16 +0100

    Use vars

--- build.sh
+++ build.sh
@@ -9,12 +9,9 @@ openwrtVersion="$1"
 arch="$2"
 archPath="$(echo \"$arch\" | sed s/-/\\//)"
 
-echo "$arch -> $archPath";
-exit 0;
-
 sudo apt install -y build-essential libncurses5-dev libncursesw5-dev zlib1g-dev gawk git gettext libssl-dev xsltproc rsync wget unzip python
 
-wget https://downloads.openwrt.org/releases/21.02.2/targets/x86/64/openwrt-imagebuilder-21.02.2-x86-64.Linux-x86_64.tar.xz
+wget https://downloads.openwrt.org/releases/$openwrtVersion/targets/$archPath/openwrt-imagebuilder-$openwrtVersion-$arch.Linux-x86_64.tar.xz
 tar -J -x -f openwrt-imagebuilder-*.tar.xz
 cd openwrt-imagebuilder-*/
 

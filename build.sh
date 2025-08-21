ee17bb6894d9505d6d8ad7017de87889725dbc8a
Justas <justas963@gmail.com>
2025-08-21 15:46:01 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -9,11 +9,11 @@ openwrtVersion="$1"
 arch="$2"
 archPath="$(echo $arch | sed s/-/\\//)"
 
-sudo apt install -y build-essential libncurses-dev zlib1g-dev gawk git gettext libssl-dev xsltproc rsync wget unzip python3 python3-distutils
+sudo apt install -y build-essential libncurses-dev zlib1g-dev gawk git gettext libssl-dev xsltproc rsync wget unzstd file python3 python3-distutils
 
-wget "https://downloads.openwrt.org/releases/$openwrtVersion/targets/$archPath/openwrt-imagebuilder-$openwrtVersion-$arch.Linux-x86_64.tar.xz"
-tar -J -x -f openwrt-imagebuilder-*.tar.xz
+wget "https://downloads.openwrt.org/releases/$openwrtVersion/targets/$archPath/openwrt-imagebuilder-$openwrtVersion-$arch.Linux-x86_64.tar.zst"
+unzstd openwrt-imagebuilder-*.tar.zst
+tar xvf     openwrt-imagebuilder-*.tar
 cd openwrt-imagebuilder-*/
 
 make image PACKAGES="luci luci-ssl qmi-utils kmod-usb-net-qmi-wwan libqmi luci-proto-qmi uqmi"
-
